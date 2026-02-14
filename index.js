c3cbe09f392431d31ed611a70801f1249462db3a
Julieth <pjimin66771106@gmail.com>
2026-02-14 12:25:58 -0500

    descripción del commit

--- index.js
+++
@@ -1,7 +0,0 @@
-function name(params) {
-    console.log("Hola:");
-}
-
-function nomina(params) {
-    console.log("nomina");
-}
--- matematica.js
+++ matematica.js
@@ -1,22 +1,50 @@
-const display = document.getElementById('display');
+let salida = document.getElementById("screen");
+let sub_salida = document.getElementById("title");
+let cambios = true ;
 
-function appendToDisplay(input) {
-    display.value += input;
-}
 
-function clearDisplay() {
-    display.value = "";
-}
+actualizarSalida(); 
+function digitos(entrada) {
 
-function deleteLast() {
-    display.value = display.value.slice(0, -1);
-}
-
-function calculate() {
-    try {
-        // eval evalúa la cadena de texto como una operación
-        display.value = eval(display.value);
-    } catch (error) {
-        display.value = "Error";
+    if (salida.textContent == "0" || cambios) {
+        salida.textContent = "";
+        cambios = false;
     }
+
+
+     salida.textContent += entrada;
+
+
+ }
+
+
+document.getElementById("delete").addEventListener("click", function() {
+     // Clears the screen content
+     salida.textContent = salida.textContent.slice(0, -1);
+     actualizarSalida();
+     
+});
+
+
+function actualizarSalida() {
+    if (!salida.textContent) {
+
+        salida.textContent = "0";
+    } 
 }
+
+
+document.getElementById("result").addEventListener("click", function () {
+  // Clears the screen content
+  sub_salida.textContent = salida.textContent;
+  salida.textContent = eval(salida.textContent);
+
+
+  
+});
+
+
+document.getElementById("clear").addEventListener("click", function () {
+  salida.textContent = "0";
+  sub_salida.textContent = "";
+});
